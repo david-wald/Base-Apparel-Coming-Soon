1c0e751d1ac9fa7f9d7f3f1c9f465778bb1e3b22
David-Wald <davidwalde81@gmail.com>
2021-11-22 10:28:57 +0100

    final

--- dist/index.643c084b.js
+++ dist/index.643c084b.js
@@ -1,2 +1,25 @@
+let input = document.querySelector('.email-input');
+const button = document.querySelector('.button');
+/* INPUT */ button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let string = input.value;
+    let value = string;
+    getValue(value);
+    input.value = '';
+});
+/* Get INPUT */ function getValue(val) {
+    return validity(val);
+}
+/* Check if valid */ function validity(val) {
+    const errorSign = document.querySelector('.error-sign-hidden');
+    const errorMessage = document.querySelector('.error-message-hidden');
+    if (val === '' || !val.includes('@')) {
+        errorSign.classList.add('error-sign-visible');
+        errorMessage.classList.add('error-message-visible');
+    } else {
+        errorSign.classList.remove('error-sign-visible');
+        errorMessage.classList.remove('error-message-visible');
+    }
+}
 
 //# sourceMappingURL=index.643c084b.js.map
